83b0639b14a0e48a7a0a7a3287c34ec87ba4f10a
zeynepidil <77980443+zeynepidil@users.noreply.github.com>
2021-02-06 17:18:53 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Musterı
+{
+	public int Id { get; set; }
+	{
+	public string Adi { get; set; }
+
+    public string Soyadi { get; set; }
+
+	public int Yas { get; set; }
+
+}
+}
